9fe2d5ab4b0540e6c11885d962bdf859d0f503bc
Syxsaran <96071669+Syxsaran@users.noreply.github.com>
2023-10-25 13:48:22 +0700

    1.0
    1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <p className={styles.centeredH}>Welcome</p>
-      <p className={styles.centeredH}>Record income and expenses</p>
+      <p className={styles.textgradient}>Welcome</p>
+      <p className={styles.textgradient}>Record income and expenses</p>
     </div>
   );
 }
