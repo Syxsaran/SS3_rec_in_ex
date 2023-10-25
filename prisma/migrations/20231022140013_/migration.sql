82d9055d3c72ed98ff10ddab0c3dc6bf84dcf29a
Syxsaran <96071669+Syxsaran@users.noreply.github.com>
2023-10-25 17:03:05 +0700

    2.0
    2.0

--- prisma/migrations/20231022140013_/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Transaction" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "userId" INTEGER NOT NULL,
-    "description" TEXT NOT NULL,
-    "amount" REAL NOT NULL
-);
